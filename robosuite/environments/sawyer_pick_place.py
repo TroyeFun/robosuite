6d2ec513e7b0ecabb044c72922fb0483470a5548
TroyeFun <953690224@qq.com>
2020-04-28 22:10:50 +0800

    set rgba color max to 3

--- robosuite/environments/sawyer_pick_place.py
+++ robosuite/environments/sawyer_pick_place.py
@@ -639,12 +639,42 @@ if __name__ == '__main__':
     import os
 
 
-    env = SawyerPickPlace(has_renderer=True,
+    env = SawyerPickPlaceCereal(has_renderer=True,
+    #env = SawyerPickPlace(has_renderer=True,
                      camera_depth=True,
                      #camera_name='birdview')
                      #camera_name='frontview')
                      camera_name='agentview')
+
+
+    env.sim.model.geom_matid[67:71] = -1   # set material to -1
+    objs = ['Milk0', 'Can0', 'Bread0', 'Cereal0']
+    color_types = ['blue', 'green', 'red', 'yellow']
+    ids = dict([(obj,env.sim.model.geom_name2id(obj)) for obj in objs])
+    obj_colors = dict(zip(objs, color_types))
+    
+    rgba_color = {
+        'blue':  [0, 0, 3, 1],  # 120,255,255
+        'green': [0, 3, 0, 1],  # 60,255,255
+        'red':   [3, 0, 0, 1], # 0, 255,255
+        'yellow':[3, 3, 0, 1],  # 30,255,255
+    }
+    hsv_range = {
+        'blue':   [[115,150,150],[125,255,255]],  # 120,255,255
+        'green':  [[55 ,150,150],[65 ,255,255]],  # 60,255,255
+        'red':    [[0  ,150,150],[10 ,255,255]],  # 0, 255,255
+        'yellow': [[25 ,150,150],[35 ,255,255]],  # 30,255,255
+
+    }
+
+    for obj in objs:
+        env.sim.model.geom_rgba[ids[obj],:] = rgba_color[obj_colors[obj]]
+
+    color_type = 'blue'
+
     while True:
+        for obj in objs:
+            env.sim.model.geom_rgba[ids[obj],:] = rgba_color[obj_colors[obj]]
         env.render()
         obs = env._get_observation()
         color, depth = obs['image'], obs['depth']
@@ -656,13 +686,15 @@ if __name__ == '__main__':
         cv2.imshow('depth', depth)
         cv2.waitKey(100)
 
-        #lower = np.array([80, 100, 100], dtype='uint8')  # h, s, v
-        #upper = np.array([124, 255, 255], dtype='uint8')
-        #hsv = cv2.cvtColor(color, cv2.COLOR_BGR2HSV)
-        #mask = cv2.inRange(hsv, lower, upper)
-        #cv2.imshow('mask', mask)
-        #cv2.waitKey(100)
+        lower, upper = np.array(hsv_range[color_type])
+        hsv = cv2.cvtColor(color, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, lower, upper)
+        cv2.imshow('mask', mask)
+        cv2.waitKey(100)
 
         import xml.etree.ElementTree as ET
+
+
+
 
         ipdb.set_trace()
